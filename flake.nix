2ddb59c6e3ced44fc9b40c18ae6f4aac287324d9
Adam Hellberg <sharparam@sharparam.com>
2025-01-21 22:49:42 +0100

    Add basic flake file

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Sharparam's Nix files";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    darwin = {
+      url = "github:LnL7/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  }
+}
