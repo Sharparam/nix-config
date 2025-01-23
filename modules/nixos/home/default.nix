d7da3cb039f599c5995126136e2a5b0fb9b69faf
Adam Hellberg <sharparam@sharparam.com>
2025-01-23 01:22:34 +0100

    Fix file options

--- modules/nixos/home/default.nix
+++ modules/nixos/home/default.nix
@@ -11,13 +11,17 @@ let
 in
 {
   options.${namespace}.home = with types; {
+    file = mkOpt attrs { } "A set of files to be managed by home-manager's `home.file`.";
+    configFile = mkOpt attrs { } "A set of files to be managed by home-manager's `xdg.configFile`.";
     extraOptions = mkOpt attrs { } "Options to pass directly to home-manager.";
   };
 
   config = {
     ${namespace}.home.extraOptions = {
       home.stateVersion = config.system.stateVersion;
+      home.file = mkAliasDefinitions options.${namespace}.home.file;
       xdg.enable = true;
+      xdg.configFile = mkAliasDefinitions options.${namespace}.home.configFile;
     };
     snowfallorg.users.${config.${namespace}.user.name}.home.config = cfg.extraOptions;
     home-manager = {
