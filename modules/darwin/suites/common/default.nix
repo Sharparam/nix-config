073d77b21c33fca3e7da66c23ea4cd8772f54407
Adam Hellberg <sharparam@sharparam.com>
2025-01-27 03:42:36 +0100

    Configure darwin input

--- modules/darwin/suites/common/default.nix
+++ modules/darwin/suites/common/default.nix
@@ -36,6 +36,7 @@ in
 
       system = {
         interface = enabled;
+        input = enabled;
       };
 
       security = {
---
+++ modules/darwin/system/input/default.nix
@@ -0,0 +1,41 @@
+{
+  lib,
+  pkgs,
+  namespace,
+  config,
+  ...
+}:
+with lib;
+with lib.${namespace};
+let
+  cfg = config.${namespace}.system.input;
+in
+{
+  options.${namespace}.system.input = with types; {
+    enable = mkEnableOption "Whether or not to enable the input system.";
+  };
+
+  config = mkIf cfg.enable {
+    system = {
+      keyboard = {
+        enableKeyMapping = true;
+        # remapCapsLockToControl = false;
+        remapCapsLockToEscape = true;
+        # swapLeftCommandAndLeftAlt = false;
+        # swapLeftCtrlAndFn = false;
+      };
+
+      defaults = {
+        NSGlobalDomain = {
+          AppleKeyboardUIMode = 3;
+
+          NSAutomaticCapitalizationEnabled = false;
+          NSAutomaticDashSubstitutionEnabled = false;
+          NSAutomaticPeriodSubstitutionEnabled = false;
+          NSAutomaticQuoteSubstitutionEnabled = false;
+          NSAutomaticSpellingCorrectionEnabled = false;
+        };
+      };
+    };
+  };
+}
