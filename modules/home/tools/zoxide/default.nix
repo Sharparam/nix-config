53efac3a3fcd4d46afe8597c2fd79b767703cff7
Adam Hellberg <sharparam@sharparam.com>
2025-01-27 04:53:03 +0100

    Fix zoxide alias

--- modules/home/tools/zoxide/default.nix
+++ modules/home/tools/zoxide/default.nix
@@ -11,22 +11,31 @@ let
   cfg = config.${namespace}.tools.zoxide;
 in
 {
-  options.${namespace}.tools.zoxide = {
+  options.${namespace}.tools.zoxide = with types; {
     enable = mkEnableOption "zoxide";
+    cmd = mkOpt (nullOr str) "j" "Zoxide command (alias) (set to null to disable)";
   };
 
   config = mkIf cfg.enable {
     programs.zoxide = {
       enable = true;
-      options = [
-        "--cmd j"
-      ];
+      options =
+        let
+          cmdArg = if (cfg.cmd == null) then "--no-cmd" else "--cmd ${cfg.cmd}";
+        in
+        [
+          cmdArg
+        ];
     };
 
-    ${namespace}.cli.aliases = {
-      pj = ''
-        pushd "$(${pkgs.zoxide}/bin/zoxide -e $@)"
-      '';
-    };
+    ${namespace}.cli.aliases =
+      let
+        zoxide = if (cfg.cmd == null) then "${pkgs.zoxide}/bin/zoxide" else cfg.cmd;
+      in
+      {
+        pj = ''
+          pushd "$(${zoxide} -e $@)"
+        '';
+      };
   };
 }
