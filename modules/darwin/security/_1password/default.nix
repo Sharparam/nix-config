7785c74335442fe914e4a9844d9246309f090e19
Adam Hellberg <sharparam@sharparam.com>
2025-02-01 18:57:55 +0100

    Update 1Password config for macOS

--- modules/darwin/security/_1password/default.nix
+++ modules/darwin/security/_1password/default.nix
@@ -18,8 +18,9 @@ in
 
   config = mkIf cfg.enable {
     homebrew = {
-      taps = [ "1password/tap" ];
-      casks = [ "1password" ];
+      # Shouldn't need the tap since 1Password seems to be in default offerings now?
+      # taps = [ "1password/tap" ];
+      casks = [ "1password" "1password-cli" ];
 
       masApps = mkIf config.${namespace}.tools.homebrew.enableMas {
         "1Password for Safari" = 1569813296;
