0bb9994495b733dfdf149ef3e4b61013aeb2a47b
Adam Hellberg <sharparam@sharparam.com>
2025-02-02 18:38:14 +0100

    Fix path to starship config

--- modules/home/tools/zsh/default.nix
+++ modules/home/tools/zsh/default.nix
@@ -9,7 +9,7 @@ with lib;
 with lib.${namespace};
 let
   cfg = config.${namespace}.tools.zsh;
-  starshipConfigPath = "${config.${namespace}.home.home}/repos/github.com/Sharparam/nix-config/dotfiles/starship/.config/starship.toml";
+  starshipConfigPath = "${config.home.homeDirectory}/repos/github.com/Sharparam/nix-config/dotfiles/starship/.config/starship.toml";
 in
 {
   options.${namespace}.tools.zsh = {
