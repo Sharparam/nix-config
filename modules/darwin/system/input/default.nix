a29b061e60eef94cf0b74c211d6d155cb493788c
Adam Hellberg <sharparam@sharparam.com>
2025-02-10 00:37:40 +0100

    Add reverse remapTilde mapping

--- modules/darwin/system/input/default.nix
+++ modules/darwin/system/input/default.nix
@@ -23,6 +23,15 @@ in {
         # swapLeftCtrlAndFn = false;
 
         nonUS.remapTilde = true;
+
+        userKeyMapping = [
+          # The reverse of remapTilde, not exposed as a convenient option for us
+          # This lets us still have access to the original key behaviour
+          {
+            HIDKeyboardModifierMappingSrc = 30064771125;
+            HIDKeyboardModifierMappingDst = 30064771172;
+          }
+        ];
       };
 
       defaults = {
