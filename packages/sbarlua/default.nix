22bededaa876ab6aba54f25a2bcae87605d99b9a
Adam Hellberg <adam.hellberg@ninetech.com>
2025-02-09 18:04:11 +0100

    Update sbarlua package

--- packages/sbarlua/default.nix
+++ packages/sbarlua/default.nix
@@ -1,46 +1,48 @@
 # https://github.com/khaneliman/khanelinix/blob/main/packages/sbarlua/default.nix
 # https://github.com/FelixKratz/SbarLua/pull/45
-
 {
   lib,
   gcc,
   readline,
-  darwin,
-  lua54Packages,
+  lua5_4,
+  # lua54Packages,
   fetchFromGitHub,
   ...
-}:
-lua54Packages.buildLuaPackage {
-  name = "sbarlua";
-  pname = "sbarlua";
-  version = "0-unstable-2024-08-12";
-  # name = "lua${lua.luaversion}-" + pname + "-" + version;
+}: let
+  lua = lua5_4;
+in
+  # lua54Packages.buildLuaPackage {
+  lua.pkgs.buildLuaPackage {
+    pname = "sbarlua";
+    version = "0-unstable-2024-08-12";
+    # name = "lua${lua.luaversion}-" + pname + "-" + version;
 
-  src = fetchFromGitHub {
-    owner = "FelixKratz";
-    repo = "SbarLua";
-    rev = "437bd2031da38ccda75827cb7548e7baa4aa9978";
-    hash = "sha256-F0UfNxHM389GhiPQ6/GFbeKQq5EvpiqQdvyf7ygzkPg=";
-  };
+    src = fetchFromGitHub {
+      owner = "FelixKratz";
+      repo = "SbarLua";
+      rev = "437bd2031da38ccda75827cb7548e7baa4aa9978";
+      hash = "sha256-F0UfNxHM389GhiPQ6/GFbeKQq5EvpiqQdvyf7ygzkPg=";
+    };
 
-  buildInputs = [
-    gcc
-    readline
-    darwin.apple_sdk.frameworks.CoreFoundation
-  ];
+    nativeBuildInputs = [
+      gcc
+    ];
 
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/lib
-    mv bin/sketchybar.so $out/lib/sketchybar.so
-    runHook postInstall
-  '';
+    buildInputs = [
+      readline
+    ];
 
-  meta = {
-    description = "A Lua API for SketchyBar";
-    homepage = "https://github.com/FelixKratz/SbarLua";
-    license = lib.licenses.gpl3Only;
-    mainProgram = "sbarlua";
-    platforms = lib.platforms.darwin;
-  };
-}
+    installPhase = ''
+      runHook preInstall
+      install -Dm755 bin/sketchybar.so $out/lib/lua/${lua.luaversion}/sketchybar.so
+      runHook postInstall
+    '';
+
+    meta = {
+      description = "A Lua API for SketchyBar";
+      homepage = "https://github.com/FelixKratz/SbarLua";
+      license = lib.licenses.gpl3Only;
+      mainProgram = "sbarlua";
+      platforms = lib.platforms.darwin;
+    };
+  }
