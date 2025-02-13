3d3de453f27f55e5dfa69e156701fb8433354405
Adam Hellberg <sharparam@sharparam.com>
2025-02-13 23:46:35 +0100

    Remove some unused yabai-helper functions

--- packages/yabai-helper/default.nix
+++ packages/yabai-helper/default.nix
@@ -60,22 +60,6 @@ in
         fi
       }
 
-      close_window() {
-        FULLSCREEN=$(yabai -m query --windows --window | jq '."is-native-fullscreen"')
-        APP=$(yabai -m query --windows --window | jq -r '."app"')
-        skhd -k "escape"
-        if [[ $FULLSCREEN = "true" ]]; then
-          # osascript -l JavaScript -e 'Application("System Events").keystroke("w",{using: ["command down", "shift down"]})'
-
-          if [[ $APP = "Firefox" ]]; then
-            hs -A -c "closeWindow()"
-          fi
-        else
-          skhd -k "shift + cmd - w"
-          # yabai -m window --close
-        fi
-      }
-
       toggle_border() {
         BORDER=$(yabai -m config window_border)
         if [[ $BORDER = "on" ]]; then
@@ -162,46 +146,6 @@ in
         yabai -m config left_padding $(echo "$PADDING"_LEFT - 10 | bc)
         yabai -m config right_padding $(echo "$PADDING"_RIGHT - 10 | bc)
         yabai -m config window_gap $(echo "$window"_GAP - 10 | bc)
-      }
-
-      new_window() {
-        APP_TO_OPEN="$1"
-        CURRENT_APP=$(yabai -m query --windows --window | jq -r '.app')
-
-        click_menu_bar() {
-          osascript -e 'tell application "System Events"' \
-            -e "tell application process \"$APP_TO_OPEN\"" \
-            -e "tell menu 1 of menu bar item 3 of menu bar 1" \
-            -e "click (first menu item whose value of attribute \"AXMenuItemCmdChar\" is \"N\" and value of attribute \"AXMenuItemCmdModifiers\" is $1)" \
-            -e 'end tell' \
-            -e 'end tell' \
-            -e 'end tell'
-        }
-
-        RUNNING=$(osascript -e "tell application \"System Events\" to set Appli_Launch to exists (processes where name is \"$APP_TO_OPEN\")")
-
-        if ! [[ $RUNNING = true ]]; then
-          if [[ $APP_TO_OPEN = "kitty" ]]; then
-            open -a kitty.app --args -1
-          fi
-          osascript -e "tell application \"$APP_TO_OPEN\" to launch"
-          exit 0
-        fi
-
-        osascript -e "tell application \"$APP_TO_OPEN\" to activate"
-
-        if [[ $2 = "stack" ]]; then
-          yabai -m window --insert stack
-        fi
-
-        if [[ $APP_TO_OPEN = "Code" ]]; then
-          click_menu_bar 1
-        elif [[ $APP_TO_OPEN = "Firefox" ]]; then
-          # HACK: yabai fails to allow firefox window to open running from command line works though
-          /Applications/Firefox.app/Contents/MacOS/firefox-bin --new-window
-        else
-          click_menu_bar 0
-        fi
       }
 
       create_spaces() {
@@ -404,18 +348,6 @@ in
           yabai -m window "$1" --toggle topmost
           echo 2 "$${PROPERTIES[0]}" "$${PROPERTIES[1]}"
         fi
-      }
-
-      set_layer() {
-        QUERY=$(yabai -m query --windows --window "$1" | jq -re '."is-topmost",."is-floating"')
-        declare -a PROPERTIES
-        PROPERTIES=("$QUERY")
-
-        if ! $${PROPERTIES[1]}; then
-          yabai -m window "$YABAI_WINDOW_ID" --layer below
-          return
-        fi
-        # yabai -m window $YABAI_WINDOW_ID --layer normal
       }
 
       auto_stack() {
