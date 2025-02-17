d01140e7101fa5761834f5ebac2a711bb1c323d8
Adam Hellberg <sharparam@sharparam.com>
2025-02-18 00:50:28 +0100

    Add some deps for emacs on darwin

--- modules/darwin/apps/emacs/default.nix
+++ modules/darwin/apps/emacs/default.nix
@@ -14,6 +14,28 @@ in {
   };
 
   config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      ## Emacs
+      binutils
+      # emacs # We're using emacs-plus from Homebrew
+
+      ## Doom
+      ripgrep
+      gnutls
+
+      ## Optional
+      fd
+      imagemagick
+      pinentry-emacs
+      zstd
+
+      ## Module deps
+      # :tools editorconfig
+      editorconfig-core-c
+      # :lang org +roam
+      sqlite
+    ];
+
     homebrew = {
       taps = ["d12frosted/emacs-plus"];
       brews = [
