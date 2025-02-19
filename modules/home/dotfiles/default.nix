272d9ab047eba570882a39d0a56a647b6591af62
Adam Hellberg <sharparam@sharparam.com>
2025-02-19 16:02:19 +0100

    Configure ideavim RC file in dotfiles config

--- modules/home/dotfiles/default.nix
+++ modules/home/dotfiles/default.nix
@@ -6,19 +6,20 @@
   ...
 }:
 with lib;
-with lib.${namespace};
-let
+with lib.${namespace}; let
   cfg = config.${namespace}.dotfiles;
-in
-{
+in {
   options.${namespace}.dotfiles = with types; {
     enable = mkEnableOption "Enable dotfiles";
     dotfilesPath =
       mkOpt str "${config.home.homeDirectory}/repos/github.com/Sharparam/nix-config/dotfiles"
-        "Path to dotfiles folder.";
+      "Path to dotfiles folder.";
   };
 
   config = mkIf cfg.enable {
-    xdg.configFile."dotfiles".source = config.lib.file.mkOutOfStoreSymlink cfg.dotfilesPath;
+    xdg.configFile = {
+      "dotfiles".source = config.lib.file.mkOutOfStoreSymlink cfg.dotfilesPath;
+      "ideavim/ideavimrc".source = config.lib.file.mkOutOfStoreSymlink "${cfg.dotfilesPath}/intellij/.ideavimrc";
+    };
   };
 }
