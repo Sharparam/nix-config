57c8215e9b09f046844e2fc48a5036d7d7ee044c
Adam Hellberg <sharparam@sharparam.com>
2025-09-17 15:08:32 +0200

    Update Lix setup

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     };
 
     lix = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.1.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
