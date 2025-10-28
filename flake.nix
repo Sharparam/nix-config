f4b6b25abf3f01975deace4869319061a8fbfddc
Adam Hellberg <sharparam@sharparam.com>
2025-10-28 13:25:06 +0100

    Fix catppuccin flake

--- flake.nix
+++ flake.nix
@@ -50,7 +50,10 @@
 
     treefmt-nix.url = "github:numtide/treefmt-nix";
 
-    catppuccin.url = "github:catppuccin/nix";
+    catppuccin = {
+      url = "github:catppuccin/nix/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     iosevka = {
       url = "github:Sharparam/Iosevka/v1.0.1";
