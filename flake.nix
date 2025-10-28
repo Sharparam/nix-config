67e0c5e74a116e8444a622d327bc296142983505
Adam Hellberg <sharparam@sharparam.com>
2025-10-28 13:29:20 +0100

    Comment unused flakes

--- flake.nix
+++ flake.nix
@@ -3,12 +3,12 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    stable.url = "github:NixOS/nixpkgs/nixos-25.05";
+    # nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    # stable.url = "github:NixOS/nixpkgs/nixos-25.05";
     # stable-darwin.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
-    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     # unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    unstable-darwin.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    # unstable-darwin.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     home-manager = {
       # url = "github:nix-community/home-manager";
