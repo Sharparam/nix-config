7639b8205ac5a78a61dc8710feabaf9c0ece8238
Adam Hellberg <sharparam@sharparam.com>
2025-12-02 17:35:53 +0100

    --amend

--- modules/home/tools/zsh/default.nix
+++ modules/home/tools/zsh/default.nix
@@ -23,7 +23,7 @@ in {
     programs = {
       zsh = {
         enable = true;
-        dotDir = ".config/zsh";
+        dotDir = "${config.xdg.configHome}/zsh";
         enableCompletion = true;
         enableVteIntegration = true;
         syntaxHighlighting.enable = !cfg.enableFastSyntaxHighlighting;
