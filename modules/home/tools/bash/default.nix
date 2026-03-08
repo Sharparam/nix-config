d33b0e9bc799e1736772e53322d94c4a1f52c04d
Adam Hellberg <sharparam@sharparam.com>
2026-03-08 23:18:20 +0100

    Add bash module

---
+++ modules/home/tools/bash/default.nix
@@ -0,0 +1,27 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+  cfg = config.snix.tools.bash;
+in
+{
+  options.snix.tools.bash = {
+    enable = lib.mkEnableOption "bash";
+    package = lib.mkPackageOption pkgs "bash" {
+      nullable = true;
+      default = "bashInteractive";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    programs.bash = {
+      enable = true;
+      package = cfg.package;
+      enableVteIntegration = true;
+    };
+  };
+}
