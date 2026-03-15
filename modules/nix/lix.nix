a72e7001a6af51ed0d89a4c19701f7426c04c5b3
Adam Hellberg <sharparam@sharparam.com>
2026-03-16 00:12:11 +0100

    Improve usage of Lix
    Sadly we had to disable the forcible blocking of CppNix since devenv
    requires it.

--- modules/nix/lix.nix
+++ modules/nix/lix.nix
@@ -1,10 +1,11 @@
+{ lib, ... }:
 {
   den.default = {
     os =
       { pkgs, ... }:
       {
         nixpkgs.overlays = [
-          (_final: prev: {
+          (final: prev: {
             inherit (prev.lixPackageSets.latest)
               nixpkgs-review
               # https://git.lix.systems/lix-project/lix/issues/980
@@ -14,7 +15,12 @@
               colmena
               ;
 
-            nixVersions = throw "CppNix bad, use Lix instead";
+            comma = prev.comma.override {
+              nix = final.lixPackageSets.latest.lix;
+            };
+
+            # Sadly we can't do this if using devenv since it's hardcoded to use CppNix
+            # nixVersions = throw "CppNix bad, use Lix instead";
             lixPackageSets = prev.lixPackageSets.extend (
               finalSet: prevSet: {
                 lix_2_93 = throw "Upgrade to Lix 2.94 or newer";
@@ -24,6 +30,12 @@
         ];
 
         nix.package = pkgs.lixPackageSets.latest.lix;
+      };
+
+    homeManager =
+      { pkgs, ... }:
+      {
+        nix.package = lib.mkDefault pkgs.lixPackageSets.latest.lix;
       };
   };
 }
