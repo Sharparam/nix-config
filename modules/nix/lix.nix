102ecace629da5044bff5692fa54177f266e9d26
Adam Hellberg <sharparam@sharparam.com>
2026-03-15 23:02:35 +0100

    Block CppNix and old Lix

--- modules/nix/lix.nix
+++ modules/nix/lix.nix
@@ -12,6 +12,13 @@
               nix-fast-build
               colmena
               ;
+
+            nixVersions = throw "CppNix bad, use Lix instead";
+            lixPackageSets = prev.lixPackageSets.extend (
+              finalSet: prevSet: {
+                lix_2_93 = throw "Upgrade to Lix 2.94 or newer";
+              }
+            );
           })
         ];
 
