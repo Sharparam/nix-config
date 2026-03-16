64e3dcb3201550d5dba3352bbb296926a2556692
Adam Hellberg <sharparam@sharparam.com>
2026-03-16 16:05:43 +0100

    Make Lix overlay work for standalone HM

--- modules/nix/lix.nix
+++ modules/nix/lix.nix
@@ -1,33 +1,42 @@
 { lib, ... }:
+let
+  overlay = final: prev: {
+    inherit (prev.lixPackageSets.latest)
+      nixpkgs-review
+      # https://git.lix.systems/lix-project/lix/issues/980
+      # nix-direnv
+      nix-eval-jobs
+      nix-fast-build
+      nurl
+      colmena
+      ;
+
+    comma = prev.comma.override {
+      nix = final.lixPackageSets.latest.lix;
+    };
+
+    # Sadly we can't do this if using devenv since it's hardcoded to use CppNix
+    # nixVersions = throw "CppNix bad, use Lix instead";
+    lixPackageSets = prev.lixPackageSets.extend (
+      finalSet: prevSet: {
+        lix_2_93 = throw "Upgrade to Lix 2.94 or newer";
+      }
+    );
+  };
+
+  hmAspect =
+    { home }:
+    {
+      homeManager.nixpkgs.overlays = [ overlay ];
+    };
+in
 {
   den.default = {
+    includes = [ hmAspect ];
     os =
       { pkgs, ... }:
       {
-        nixpkgs.overlays = [
-          (final: prev: {
-            inherit (prev.lixPackageSets.latest)
-              nixpkgs-review
-              # https://git.lix.systems/lix-project/lix/issues/980
-              # nix-direnv
-              nix-eval-jobs
-              nix-fast-build
-              colmena
-              ;
-
-            comma = prev.comma.override {
-              nix = final.lixPackageSets.latest.lix;
-            };
-
-            # Sadly we can't do this if using devenv since it's hardcoded to use CppNix
-            # nixVersions = throw "CppNix bad, use Lix instead";
-            lixPackageSets = prev.lixPackageSets.extend (
-              finalSet: prevSet: {
-                lix_2_93 = throw "Upgrade to Lix 2.94 or newer";
-              }
-            );
-          })
-        ];
+        nixpkgs.overlays = [ overlay ];
 
         nix.package = pkgs.lixPackageSets.latest.lix;
       };
