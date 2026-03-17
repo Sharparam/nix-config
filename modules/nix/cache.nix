419b74ac2e5b39ae70f02f70cb09c781fa1d1ba3
Adam Hellberg <sharparam@sharparam.com>
2026-03-17 10:25:18 +0100

    Configure substituters for home-manager as well
    Signed-off-by: Adam Hellberg <sharparam@sharparam.com>
    Change-Id: Ibd553f3776e0bfcc053572425198ca336a6a6964

--- modules/nix/cache.nix
+++ modules/nix/cache.nix
@@ -1,15 +1,27 @@
+let
+  substituters = [
+    "https://nix-community.cachix.org"
+  ];
+
+  trusted-public-keys = [
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  ];
+
+  settings = {
+    inherit substituters trusted-public-keys;
+  };
+in
 {
   den.default = {
     os = {
       nix = {
-        settings = {
-          substituters = [
-            "https://nix-community.cachix.org"
-          ];
-          trusted-public-keys = [
-            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-          ];
-        };
+        inherit settings;
+      };
+    };
+
+    homeManager = {
+      nix = {
+        inherit settings;
       };
     };
   };
