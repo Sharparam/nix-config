d3b45f9b1cc523bb54ee8a7e969eede33adf5fb7
Adam Hellberg <sharparam@sharparam.com>
2026-03-17 17:17:55 +0100

    Fix NixOS cache missing in HM config

--- modules/nix/cache.nix
+++ modules/nix/cache.nix
@@ -7,8 +7,21 @@ let
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
   ];
 
+  extra-home-substituters = [
+    "https://cache.nixos.org"
+  ];
+
+  extra-home-trusted-public-keys = [
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+  ];
+
   settings = {
     inherit substituters trusted-public-keys;
+  };
+
+  homeSettings = {
+    substituters = substituters ++ extra-home-substituters;
+    trusted-public-keys = trusted-public-keys ++ extra-home-trusted-public-keys;
   };
 in
 {
@@ -21,7 +34,7 @@ in
 
     homeManager = {
       nix = {
-        inherit settings;
+        settings = homeSettings;
       };
     };
   };
