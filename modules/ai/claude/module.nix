f859234887a6fba9c5a182ee0a10029f4025257e
Adam Hellberg <sharparam@sharparam.com>
2026-03-20 16:42:12 +0100

    Switch to native binary version of claude-code
    Signed-off-by: Adam Hellberg <sharparam@sharparam.com>
    Change-Id: I0772270afb01ce150ae4d00e5e0bb5286a6a6964

--- modules/ai/claude/module.nix
+++ modules/ai/claude/module.nix
@@ -1,13 +1,29 @@
+let
+  substituters = [ "https://claude-code.cachix.org" ];
+  trusted-public-keys = [ "claude-code.cachix.org-1:YeXf2aNu7UTX8Vwrze0za1WEDS+4DuI2kVeWEE4fsRk=" ];
+  nix.settings = {
+    inherit substituters trusted-public-keys;
+  };
+in
 {
   den.aspects.ai.provides.claude = {
+    os = {
+      inherit nix;
+    };
+
     darwin = {
       homebrew.casks = [ "claude" ];
     };
 
-    homeManager = {
-      programs.claude-code = {
-        enable = true;
+    homeManager =
+      { pkgs, ... }:
+      {
+        inherit nix;
+
+        programs.claude-code = {
+          enable = true;
+          package = pkgs.claude-code-bin;
+        };
       };
-    };
   };
 }
