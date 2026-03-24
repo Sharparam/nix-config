4c2e97170f1eb431c0368556d585671fc2e1cfdf
Adam Hellberg <sharparam@sharparam.com>
2026-03-24 15:46:15 +0100

    Add devenv cachix
    Signed-off-by: Adam Hellberg <sharparam@sharparam.com>
    Change-Id: I58acf4539ca749503e9706b4ddd654026a6a6964

--- modules/nix/devenv.nix
+++ modules/nix/devenv.nix
@@ -1,8 +1,21 @@
+let
+  substituters = [ "https://devenv.cachix.org" ];
+  trusted-public-keys = [ "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=" ];
+  nix.settings = {
+    inherit substituters trusted-public-keys;
+  };
+in
 {
   den.default = {
+    os = {
+      inherit nix;
+    };
+
     homeManager =
       { pkgs, ... }:
       {
+        inherit nix;
+
         home.packages = [ pkgs.devenv ];
       };
   };
