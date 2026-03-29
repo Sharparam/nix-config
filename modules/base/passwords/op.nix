4d3640e4891bb0a974e702864de4cde02ffe27fa
Adam Hellberg <sharparam@sharparam.com>
2026-03-29 20:09:47 +0200

    Configure 1Password SSH agent
    Signed-off-by: Adam Hellberg <sharparam@sharparam.com>
    Change-Id: I9396bc6c70de9e02126f5bb047a275ac6a6a6964

--- modules/base/passwords/op.nix
+++ modules/base/passwords/op.nix
@@ -46,17 +46,46 @@
     };
 
     homeManager =
-      { lib, ... }:
+      {
+        config,
+        lib,
+        ...
+      }:
       let
         sshAuthSock = "$HOME/.1password/agent.sock";
         opPlugins = "$HOME/.config/op/plugins.sh";
         sessionVariables = {
           SSH_AUTH_SOCK = sshAuthSock;
         };
+        sops = config.sops;
       in
       {
         home.sessionVariables = sessionVariables;
         systemd.user.sessionVariables = sessionVariables;
+
+        sops = {
+          secrets = {
+            op-a-family-id = { };
+            op-a-family-v-private-id = { };
+            op-a-work-id = { };
+            op-a-work-v-employee-id = { };
+          };
+
+          templates."op-ssh-agent.toml" = {
+            path = "${config.xdg.configHome}/1Password/ssh/agent.toml";
+            content = ''
+              [[ssh-keys]]
+              account = "${sops.placeholder.op-a-family-id}"
+              vault = "${sops.placeholder.op-a-family-v-private-id}"
+
+              [[ssh-keys]]
+              account = "${sops.placeholder.op-a-work-id}"
+              vault = "${sops.placeholder.op-a-work-v-employee-id}"
+            '';
+          };
+        };
+
+        # xdg.configFile."1Password/ssh/agent.toml".source = sops.templates."op-ssh-agent.toml".path;
 
         programs = {
           ssh = {
--- modules/users/sharparam/sops.nix
+++ modules/users/sharparam/sops.nix
@@ -2,9 +2,11 @@
   den.aspects.sharparam.provides.secrets = {
     homeManager = {
       sops.secrets = {
-        nix-access-tokens = {
-          sopsFile = ./secrets.yaml;
-        };
+        nix-access-tokens.sopsFile = ./secrets.yaml;
+        op-a-family-id.sopsFile = ./secrets.yaml;
+        op-a-family-v-private-id.sopsFile = ./secrets.yaml;
+        op-a-work-id.sopsFile = ./secrets.yaml;
+        op-a-work-v-employee-id.sopsFile = ./secrets.yaml;
       };
     };
   };
