471052feb3c5d1e685da23f510c950c80c058874
Adam Hellberg <sharparam@sharparam.com>
2026-03-29 04:07:58 +0200

    Fix custom en_SE locale in standalone home manager
    Signed-off-by: Adam Hellberg <sharparam@sharparam.com>
    Change-Id: I51e661d8f05b01ed8f92d24c7ad7be2b6a6a6964

--- modules/defaults/locale.nix
+++ modules/defaults/locale.nix
@@ -6,6 +6,32 @@
   };
 
   den.default = {
+    includes = [
+      (
+        { home }:
+        {
+          homeManager =
+            { inputs', ... }:
+            let
+              glibcLocales = inputs'.locale-en_se.packages.glibcLocales;
+            in
+            {
+              i18n.glibcLocales = glibcLocales.override {
+                allLocales = false;
+                locales = [
+                  "C.UTF-8/UTF-8"
+                  "en_DK.UTF-8/UTF-8"
+                  "en_GB.UTF-8/UTF-8"
+                  "en_SE.UTF-8/UTF-8"
+                  "en_US.UTF-8/UTF-8"
+                  "sv_SE.UTF-8/UTF-8"
+                ];
+              };
+            };
+        }
+      )
+    ];
+
     nixos = {
       imports = [
         inputs.locale-en_se.nixosModules.default
