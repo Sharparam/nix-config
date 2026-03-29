3e67c06fe1e2b3541be6cd8fb87bb7a6b2bc9880
Adam Hellberg <sharparam@sharparam.com>
2026-03-30 01:27:42 +0200

    Update GitHub configuration
    Signed-off-by: Adam Hellberg <sharparam@sharparam.com>
    Change-Id: Ifcfa58f40396c8531b6327116a2b2e116a6a6964

--- modules/base/vcs/git/github.nix
+++ modules/base/vcs/git/github.nix
@@ -17,10 +17,7 @@ in
         home.sessionVariables = sessionVariables;
         systemd.user.sessionVariables = sessionVariables;
 
-        home.packages = [
-          pkgs.gh
-          pkgs.github-copilot-cli
-        ];
+        home.packages = [ pkgs.github-copilot-cli ];
 
         programs = {
           zsh.sessionVariables = sessionVariables;
@@ -38,19 +35,41 @@ in
               "ssh://git@github.com".pushInsteadOf = "https://github.com";
             };
           };
+
+          gh = {
+            enable = true;
+            gitCredentialHelper = {
+              # enable = lib.mkDefault true;
+            };
+            settings = {
+              browser = "";
+              color_labels = "enabled";
+              editor = "";
+              git_protocol = "ssh";
+              http_unix_socket = "";
+              pager = "";
+              prompt = "enabled";
+              aliases = {
+                clone = "repo clone";
+                co = "pr checkout";
+              };
+            };
+          };
         };
 
-        home.shellAliases =
-          let
-            gh = "gh";
-          in
-          {
-            "?" = "${gh} copilot suggest -t shell";
-            "??" = "${gh} copilot explain";
-            "?e" = "${gh} copilot explain";
-            "?g" = "${gh} copilot suggest -t git";
-            "?gh" = "${gh} copilot suggest -t gh";
-          };
+        # The gh-copilot extension for gh has been deprecated
+        # Now we have to ask the copilot CLI interactively instead
+        # home.shellAliases =
+        #   let
+        #     gh = "gh";
+        #   in
+        #   {
+        #     "?" = "${gh} copilot suggest -t shell";
+        #     "??" = "${gh} copilot explain";
+        #     "?e" = "${gh} copilot explain";
+        #     "?g" = "${gh} copilot suggest -t git";
+        #     "?gh" = "${gh} copilot suggest -t gh";
+        #   };
       };
   };
 }
