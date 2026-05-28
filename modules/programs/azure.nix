9b24391f5c4b3782dd2bbf7f6b5082a1c69e3818
Adam Hellberg <sharparam@sharparam.com>
2026-05-28 17:11:53 +0200

    Add Bicep CLI to Azure programs
    Signed-off-by: Adam Hellberg <sharparam@sharparam.com>
    Change-Id: Ie94de1ca0c70b45f375c6419411bba416a6a6964

--- modules/programs/azure.nix
+++ modules/programs/azure.nix
@@ -12,6 +12,10 @@
           inherit (pkgs.azure-cli-extensions)
             account
             ;
+
+          # TODO (2026-05-28): Replace with version in nixpkgs when it stops being ancient
+          # See: https://github.com/NixOS/nixpkgs/pull/469509
+          bicep = pkgs.local.bicep;
         };
       };
   };
