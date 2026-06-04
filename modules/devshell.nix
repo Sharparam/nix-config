a062cfbd2a33e45f7ec280ce6537787d921cd4e5
Adam Hellberg <sharparam@sharparam.com>
2026-06-04 21:26:48 +0200

    Update devshell
    Signed-off-by: Adam Hellberg <sharparam@sharparam.com>
    Change-Id: Ib85af30cfa7e3f1faf52682b4edab64f6a6a6964

--- modules/devshell.nix
+++ modules/devshell.nix
@@ -3,10 +3,12 @@
     { pkgs, ... }:
     {
       devShells.default = pkgs.mkShell {
-        nativeBuildInputs = builtins.attrValues {
+        packages = builtins.attrValues {
           inherit (pkgs)
             just
             nh
+            oxfmt
+            oxlint
             ;
         };
       };
