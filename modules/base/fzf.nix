0f50cabcd7c105a450d7001b7222e5baa81b75cc
Adam Hellberg <sharparam@sharparam.com>
2026-08-04 17:08:34 +0200

    Update fzf settings
    Signed-off-by: Adam Hellberg <sharparam@sharparam.com>
    Change-Id: I0785abaebabb7fd8c7ce2b78527e3dd56a6a6964

--- modules/base/fzf.nix
+++ modules/base/fzf.nix
@@ -8,8 +8,9 @@
         {
           enable = true;
           defaultCommand = "${fd} --type f --hidden --exclude \".git\"";
-          changeDirWidgetCommand = "${fd} --type d --hidden --exclude \".git\"";
-          fileWidgetCommand = "${fd} --type f --hidden --exclude \".git\"";
+          changeDirWidget.command = "${fd} --type d --hidden --exclude \".git\"";
+          fileWidget.command = "${fd} --type f --hidden --exclude \".git\"";
+          historyWidget.command = ""; # We use atuin instead
           tmux.enableShellIntegration = true;
         };
     };
