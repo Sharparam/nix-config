48c5787f1b309cdd7785d8afa5a7fbb80ba56890
Adam Hellberg <sharparam@sharparam.com>
2026-09-20 21:58:16 +0200

    Make my radicle node the only preferred one
    Signed-off-by: Adam Hellberg <sharparam@sharparam.com>
    Change-Id: Id8b6b2c4b107938f6b6f74ad6ce9cca06a6a6964

--- modules/base/vcs/radicle.nix
+++ modules/base/vcs/radicle.nix
@@ -37,11 +37,11 @@ in
             };
             preferredSeeds = [
               "z6MkoGNAbLuAjQoJBSiwvmoGAM9CDp8idGjiABJEBUQyucqs@seed.rad.sharparam.com:8776"
-              "z6MkrLMMsiPWUcNPHcRajuMi9mDfYckSoJyPwwnknocNYPm7@iris.radicle.network:8776"
-              "z6MkrLMMsiPWUcNPHcRajuMi9mDfYckSoJyPwwnknocNYPm7@irisradizskwweumpydlj4oammoshkxxjur3ztcmo7cou5emc6s5lfid.onion:8776"
-              "z6Mkmqogy2qEM2ummccUthFEaaHvyYmYBYh3dbe9W4ebScxo@rosa.radicle.network:8776"
-              "z6Mkmqogy2qEM2ummccUthFEaaHvyYmYBYh3dbe9W4ebScxo@rosarad5bxgdlgjnzzjygnsxrwxmoaj4vn7xinlstwglxvyt64jlnhyd.onion:8776"
-              "z6MkuyPDChcwCruKGd2tw54FZo2HemY135JvTLzbVTvoWKJR@index.radicle.garden:8776"
+              # "z6MkrLMMsiPWUcNPHcRajuMi9mDfYckSoJyPwwnknocNYPm7@iris.radicle.network:8776"
+              # "z6MkrLMMsiPWUcNPHcRajuMi9mDfYckSoJyPwwnknocNYPm7@irisradizskwweumpydlj4oammoshkxxjur3ztcmo7cou5emc6s5lfid.onion:8776"
+              # "z6Mkmqogy2qEM2ummccUthFEaaHvyYmYBYh3dbe9W4ebScxo@rosa.radicle.network:8776"
+              # "z6Mkmqogy2qEM2ummccUthFEaaHvyYmYBYh3dbe9W4ebScxo@rosarad5bxgdlgjnzzjygnsxrwxmoaj4vn7xinlstwglxvyt64jlnhyd.onion:8776"
+              # "z6MkuyPDChcwCruKGd2tw54FZo2HemY135JvTLzbVTvoWKJR@index.radicle.garden:8776"
             ];
             publicExplorer = "https://rad.sharparam.com/nodes/$host/$rid$path";
           };
